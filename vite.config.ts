5a21ca70fee5db139897bcbcb3b1aa676a5013a4
Zarskiii <130418933+Zarskiii@users.noreply.github.com>
2025-05-12 13:09:08 -0600

    fix: Update base URL to match repository name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/zars-tiki-catering/",
+  base: "/tiki-catering-clone/",
   server: {
     host: "::",
     port: 8081,
